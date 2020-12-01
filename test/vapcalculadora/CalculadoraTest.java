234b4388479da06e5c042234af37379d955f651f
NENE1 <NENE1@192.168.9.241>
2020-12-01 10:26:40 +0100

    Borrado de las llamadas a fail por Pedro Alberto Valero Avarado

--- test/vapcalculadora/CalculadoraTest.java
+++ test/vapcalculadora/CalculadoraTest.java
@@ -7,10 +7,12 @@ package vapcalculadora;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 
 /**
  *
